1e3829b58e0deec94aaf1f11041ed83ea149b34d
baoyachi <liaoymxsdl@sina.com>
2016-06-22 18:50:00 +0800

    add difference StepView

--- app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
+++ app/src/main/java/com/baoyachi/stepview/demo/MainActivity.java
@@ -21,17 +21,97 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StepView setpview = (StepView) findViewById(R.id.step_flow);
+        showSetpView0();
+        showSetpView1();
+        showSetpView2();
+        showSetpView3();
+        showSetpView4();
+        showSetpView5();
+        showSetpView6();
+    }
 
-        List<String> list6 = new ArrayList<>();
-        list6.add("接单");
-        list6.add("出发");
-        list6.add("开始");
-        list6.add("结束");
-        list6.add("付账");
-        list6.add("完成");
-        setpview.setStepsViewIndicatorComplectingPosition(4)//设置完成的步数
-                .setStepViewTexts(list6)//总步骤
+    private void showSetpView0()
+    {
+        StepView setpview0 = (StepView) findViewById(R.id.step_view0);
+        List<String> list0 = new ArrayList<>();
+        list0.add("接单");
+        list0.add("出发");
+        list0.add("开始");
+        list0.add("结束");
+        list0.add("完成");
+        setpview0.setStepsViewIndicatorComplectingPosition(2)//设置完成的步数
+                .setStepViewTexts(list0)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
+    }
+
+
+    private void showSetpView1()
+    {
+        List<String> list1 = new ArrayList<>();
+        list1.add("接单");
+        StepView setpview1 = (StepView) findViewById(R.id.step_view1);
+        setpview1.setStepsViewIndicatorComplectingPosition(0)//设置完成的步数
+                .setStepViewTexts(list1)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
+    }
+
+    private void showSetpView2()
+    {
+        List<String> list2 = new ArrayList<>();
+        list2.add("接单");
+        list2.add("出发");
+        StepView setpview2 = (StepView) findViewById(R.id.step_view2);
+        setpview2.setStepsViewIndicatorComplectingPosition(0)//设置完成的步数
+                .setStepViewTexts(list2)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
+    }
+
+    private void showSetpView3()
+    {
+        List<String> list3 = new ArrayList<>();
+        list3.add("接单");
+        list3.add("出发");
+        list3.add("开始");
+        StepView setpview3 = (StepView) findViewById(R.id.step_view3);
+        setpview3.setStepsViewIndicatorComplectingPosition(1)//设置完成的步数
+                .setStepViewTexts(list3)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
+    }
+
+    private void showSetpView4()
+    {
+        List<String> list4 = new ArrayList<>();
+        list4.add("接单");
+        list4.add("出发");
+        list4.add("开始");
+        list4.add("结束");
+        StepView setpview4 = (StepView) findViewById(R.id.step_view4);
+        setpview4.setStepsViewIndicatorComplectingPosition(2)//设置完成的步数
+                .setStepViewTexts(list4)//总步骤
                 .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
                 .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
                 .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
@@ -40,39 +120,50 @@ public class MainActivity extends AppCompatActivity
                 .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
                 .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
 
+
     }
 
-    void setData()
+    private void showSetpView5()
     {
-        List<String> list1 = new ArrayList<>();
-        list1.add("接单");
-        List<String> list2 = new ArrayList<>();
-        list2.add("接单");
-        list2.add("出发");
-
-        List<String> list3 = new ArrayList<>();
-        list3.add("接单");
-        list3.add("出发");
-        list3.add("开始");
-
-        List<String> list4 = new ArrayList<>();
-        list4.add("接单");
-        list4.add("出发");
-        list4.add("开始");
-        list4.add("结束");
-
         List<String> list5 = new ArrayList<>();
         list5.add("接单");
         list5.add("出发");
         list5.add("开始");
         list5.add("结束");
         list5.add("结账");
+        StepView setpview5 = (StepView) findViewById(R.id.step_view5);
+        setpview5.setStepsViewIndicatorComplectingPosition(3)//设置完成的步数
+                .setStepViewTexts(list5)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
+    }
+
+    private void showSetpView6()
+    {
+        StepView setpview6 = (StepView) findViewById(R.id.step_view6);
+
         List<String> list6 = new ArrayList<>();
         list6.add("接单");
         list6.add("出发");
         list6.add("开始");
         list6.add("结束");
-        list6.add("结账");
+        list6.add("付账");
         list6.add("完成");
+        setpview6.setStepsViewIndicatorComplectingPosition(4)//设置完成的步数
+                .setStepViewTexts(list6)//总步骤
+                .setStepsViewIndicatorCompletedLineColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsViewIndicator完成线的颜色
+                .setStepsViewIndicatorUnCompletedLineColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsViewIndicator未完成线的颜色
+                .setStepViewComplectedTextColor(ContextCompat.getColor(getBaseContext(), android.R.color.white))//设置StepsView text完成线的颜色
+                .setStepViewUnComplectedTextColor(ContextCompat.getColor(getBaseContext(), R.color.uncompleted_text_color))//设置StepsView text未完成线的颜色
+                .setStepsViewIndicatorCompleteIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.complted))//设置StepsViewIndicator CompleteIcon
+                .setStepsViewIndicatorDefaultIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.default_icon))//设置StepsViewIndicator DefaultIcon
+                .setStepsViewIndicatorAttentionIcon(ContextCompat.getDrawable(getBaseContext(), R.drawable.attention));//设置StepsViewIndicator AttentionIcon
     }
+
+
 }
